f3f40dd107dfa270cb01a07fcb05ff77b53fd18d
fatalmb14 <66944584+fatalmb14@users.noreply.github.com>
2021-09-30 17:08:48 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+ #include <iostream>
+ #include <cstdlib>
+ using namespace std;
+ 
+ int main()
+ { 
+  std::cout <<"hello world" <<std ::endl;
+   
+   system("PAUSE");
+ 
+   
+   
+   
+   
+   
+   return 0;
+
+}
